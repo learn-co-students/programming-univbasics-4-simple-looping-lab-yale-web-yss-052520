303a8e20a98438daf23bd49b9db63e1f5ce7e02d
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-03 22:12:32 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,33 @@
 # Write your methods here
+
+def loop_message_five_times(str)
+  counter = 0 
+  while counter < 5 do 
+    puts str 
+    counter += 1
+  end
+end
+
+def loop_message_n_times(str, n)
+  while n > 0 do 
+    puts str 
+    n -= 1 
+  end 
+end 
+
+def output_array(arr)
+  counter = 0 
+  while counter < arr.length do 
+    puts arr[counter]
+    counter += 1
+  end
+end 
+
+def return_string_array(arr)
+  counter = 0 
+  while counter < arr.length do 
+    arr[counter] = arr[counter].to_s
+    counter += 1
+  end 
+  arr 
+end 
